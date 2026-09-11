da6be62e005bd4d28f0c6bce93701c982931abe0
Sergio Velez <125153495+Sjvr21@users.noreply.github.com>
2026-09-11 18:33:24 -0400

    Map marketplace orders reviews and media fields

--- backend/BoricuaBite.Infrastructure/Persistence/BoricuaBiteDbContext.cs
+++ backend/BoricuaBite.Infrastructure/Persistence/BoricuaBiteDbContext.cs
@@ -11,6 +11,9 @@ public sealed class BoricuaBiteDbContext(DbContextOptions<BoricuaBiteDbContext> 
 {
     public DbSet<Restaurant> Restaurants => Set<Restaurant>();
     public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+    public DbSet<MarketplaceOrder> MarketplaceOrders => Set<MarketplaceOrder>();
+    public DbSet<MarketplaceOrderItem> MarketplaceOrderItems => Set<MarketplaceOrderItem>();
+    public DbSet<RestaurantReview> RestaurantReviews => Set<RestaurantReview>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -22,6 +25,10 @@ public sealed class BoricuaBiteDbContext(DbContextOptions<BoricuaBiteDbContext> 
         restaurant.Property(x => x.Description).HasMaxLength(2000);
         restaurant.Property(x => x.PhoneNumber).HasMaxLength(30);
         restaurant.Property(x => x.LogoUrl).HasMaxLength(2048);
+        restaurant.Property(x => x.CoverImageUrl).HasMaxLength(2048);
+        restaurant.Property(x => x.StripeConnectedAccountId).HasMaxLength(255);
+        restaurant.Property(x => x.StripeSubscriptionId).HasMaxLength(255);
+        restaurant.Property(x => x.SubscriptionStatus).HasConversion<string>().HasMaxLength(32);
         restaurant.HasOne<ApplicationUser>().WithMany().HasForeignKey(x => x.OwnerId)
             .OnDelete(DeleteBehavior.Restrict);
         restaurant.OwnsOne(x => x.Address, address =>
@@ -39,8 +46,39 @@ public sealed class BoricuaBiteDbContext(DbContextOptions<BoricuaBiteDbContext> 
         menuItem.HasQueryFilter(x => !x.IsDeleted);
         menuItem.Property(x => x.RestaurantId);
         menuItem.Property(x => x.Name).HasMaxLength(150).IsRequired();
+        menuItem.Property(x => x.Description).HasMaxLength(1000);
+        menuItem.Property(x => x.ImageUrl).HasMaxLength(2048);
         menuItem.Property(x => x.Price).HasPrecision(12, 2);
         menuItem.HasOne<Restaurant>().WithMany().HasForeignKey(x => x.RestaurantId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        var order = builder.Entity<MarketplaceOrder>();
+        order.HasQueryFilter(x => !x.IsDeleted);
+        order.Property(x => x.Status).HasConversion<string>().HasMaxLength(32);
+        order.Property(x => x.PaymentMethod).HasConversion<string>().HasMaxLength(32);
+        order.Property(x => x.PaymentStatus).HasConversion<string>().HasMaxLength(32);
+        order.Property(x => x.Subtotal).HasPrecision(12, 2);
+        order.Property(x => x.TaxAmount).HasPrecision(12, 2);
+        order.Property(x => x.PlatformFee).HasPrecision(12, 2);
+        order.Property(x => x.Total).HasPrecision(12, 2);
+        order.Property(x => x.Currency).HasMaxLength(3);
+        order.Property(x => x.StripeCheckoutSessionId).HasMaxLength(255);
+        order.HasIndex(x => x.StripeCheckoutSessionId).IsUnique();
+        order.HasOne<Restaurant>().WithMany().HasForeignKey(x => x.RestaurantId).OnDelete(DeleteBehavior.Restrict);
+        order.HasOne<ApplicationUser>().WithMany().HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict);
+        order.HasMany(x => x.Items).WithOne().HasForeignKey(x => x.OrderId).OnDelete(DeleteBehavior.Cascade);
+
+        var orderItem = builder.Entity<MarketplaceOrderItem>();
+        orderItem.HasKey(x => x.Id);
+        orderItem.Property(x => x.Name).HasMaxLength(150).IsRequired();
+        orderItem.Property(x => x.UnitPrice).HasPrecision(12, 2);
+
+        var review = builder.Entity<RestaurantReview>();
+        review.HasQueryFilter(x => !x.IsDeleted);
+        review.Property(x => x.Comment).HasMaxLength(2000);
+        review.HasIndex(x => x.OrderId).IsUnique();
+        review.HasOne<Restaurant>().WithMany().HasForeignKey(x => x.RestaurantId).OnDelete(DeleteBehavior.Restrict);
+        review.HasOne<ApplicationUser>().WithMany().HasForeignKey(x => x.CustomerId).OnDelete(DeleteBehavior.Restrict);
+        review.HasOne<MarketplaceOrder>().WithOne().HasForeignKey<RestaurantReview>(x => x.OrderId).OnDelete(DeleteBehavior.Restrict);
     }
 }
